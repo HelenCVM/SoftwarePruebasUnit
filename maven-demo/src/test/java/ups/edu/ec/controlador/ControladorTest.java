3974899d4a6a4399ccd95a3fd0cd9fff33575dcd
Cinthia iza <cizac1@est.ups.edu.ec>
2020-06-18 14:10:54 -0500

    Cambio

--- maven-demo/src/test/java/ups/edu/ec/controlador/ControladorTest.java
+++ maven-demo/src/test/java/ups/edu/ec/controlador/ControladorTest.java
@@ -7,12 +7,73 @@ import java.util.ArrayList;
 
 import org.junit.Test;
 
+import ups.edu.ec.modelo.contabilidad.HistorialAhorro;
+import ups.edu.ec.modelo.transaccion.CuentaAhorro;
 import ups.edu.ec.modelo.usuario.Socio;
 
 public class ControladorTest {
-
+	CuentaAhorro cuenta= new CuentaAhorro();
 	controlador con=new controlador();
+	Socio socio=new Socio();
+	/**
+	 * Modulo Socio
+	 */
+	@Test
+	public void testMostrarListaSocio() {
+		
+		socio.setCod(1);
+		socio.setNombreCompleto("Juan Sebastian");
+		socio.setApellidosCompletos("Vasquez");
+		socio.setCorreo("juanv@gmail.com");
+		socio.setContraseña("1234");
+		String esperado="1,Juan Sebastian,Vasquez,juanv@gmail.com,1234";
+		String resultado=con.mostrarListaSocio(socio);
+		assertEquals(esperado,resultado,0);
+	}
+
+	@Test
+	public void testEliminarSocio() {
+		Socio so=new Socio();
+		so.setCod(1);
+		so.setNombreCompleto("Juan Sebastian");
+		so.setApellidosCompletos("Vasquez");
+		so.setCorreo("juanv@gmail.com");
+		so.setContraseña("1234");
+		ArrayList esperado1= new ArrayList();
+		ArrayList resultado1=con.eliminarSocio(so);
+		//assertArrayEquals(esperado1, resultado1);
 	
+	}
+
+	@Test
+	public void testModificarSocio() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIngresarSocio() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testIniciarSesion() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testSolicitarCredito() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testAgregarEstadoCuenta() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCalcularSaldo() {
+		fail("Not yet implemented");
+	}
 	@Test
 	public void testControlador() {
 		fail("Not yet implemented");
@@ -20,8 +81,16 @@ public class ControladorTest {
 
 	@Test
 	public void testAgregarHistorial() {
-		fail("Not yet implemented");
-		
+	HistorialAhorro historial= new HistorialAhorro();
+	historial.setFecha("11/11/2012");
+	historial.setId(1);	
+	
+	cuenta.setCapital(100);
+	cuenta.setNumeroCuenta("22335566");
+	cuenta.setSaldo(100);
+	cuenta.setSocio(socio);
+	historial.setCuentaahorro(cuenta);
+	con.agregarHistorial(historial);
 	}
 
 	@Test
@@ -86,63 +155,6 @@ public class ControladorTest {
 
 	@Test
 	public void testCalcularIngreso() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testMostrarListaSocio() {
-		Socio so=new Socio();
-		so.setCod(1);
-		so.setNombreCompleto("Juan Sebastian");
-		so.setApellidosCompletos("Vasquez");
-		so.setCorreo("juanv@gmail.com");
-		so.setContraseña("1234");
-		String esperado="1,Juan Sebastian,Vasquez,juanv@gmail.com,1234";
-		String resultado=con.mostrarListaSocio(so);
-		assertEquals(esperado,resultado,0);
-	}
-
-	@Test
-	public void testEliminarSocio() {
-		Socio so=new Socio();
-		so.setCod(1);
-		so.setNombreCompleto("Juan Sebastian");
-		so.setApellidosCompletos("Vasquez");
-		so.setCorreo("juanv@gmail.com");
-		so.setContraseña("1234");
-		ArrayList esperado1= new ArrayList();
-		ArrayList resultado1=con.eliminarSocio(so);
-		//assertArrayEquals(esperado1, resultado1);
-	
-	}
-
-	@Test
-	public void testModificarSocio() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testIngresarSocio() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testIniciarSesion() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testSolicitarCredito() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testAgregarEstadoCuenta() {
-		fail("Not yet implemented");
-	}
-
-	@Test
-	public void testCalcularSaldo() {
 		fail("Not yet implemented");
 	}
 
